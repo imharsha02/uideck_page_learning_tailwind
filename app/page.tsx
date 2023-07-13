ea3dd845ed7dd4f7fb7a173df90933aa0a6f8da8
G. H. Krishna Chaitanya <g.h.krishnachaitanya@Gs-Mac-mini.local>
2023-07-13 10:10:18 +0530

    Back to the begining

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,5 @@
 import React from 'react'
 import Header from '../components/Header'
-<<<<<<< HEAD
-import  Main  from '../components/Main'
-
-const Home = () => {
-  return (
-    <div className='max-w-7xl '>
-      <Header />
-      <Main />
-    </div>
-  )
-}
-
-export default Home
-=======
 
 const Home = () => {
   return (
@@ -24,4 +10,4 @@ const Home = () => {
 }
 
 export default Home
->>>>>>> d7802562f07fbe9060afca964e44d55d2160a0c4
+
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Navbar from './Navbar'
 
 const Header = () => {
@@ -10,25 +9,3 @@ const Header = () => {
 }
 
 export default Header
-=======
-import Link from 'next/link'
-
-const Header = () => {
-  return (
-    <div className='flex justify-evenly shadow-sm p-10'>
-        <Link href="/">Nextbase</Link>
-        <ul className='flex'>
-            <li>Features</li>
-            <li>Demos</li>
-            <li>Pricing</li>
-            <li>Discord</li>
-            <li>public Roadmap</li>
-            <li>FAQ</li>
-        </ul>
-        <Link href="/account">Account</Link>
-    </div>
-  )
-}
-
-export default Header
->>>>>>> d7802562f07fbe9060afca964e44d55d2160a0c4
